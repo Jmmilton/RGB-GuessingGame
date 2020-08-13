c1baff64b3a66ed942c4c71ff12ca26b39b20bb0
Jay Milton <jay@Jays-MacBook-Pro.local>
2020-08-13 16:44:57 -0400

    logic, pick correct color, throw alert..

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ const squares = document.querySelectorAll(".square");
 const pickedColour = colours[3];
 const colourDisplay = document.getElementById('colourDisplay')
 
-colourDisplay.textContent = pickedColour
+colourDisplay.textContent = pickedColour;
 
 for(var i = 0; i < squares.length; i++){
+    //add initial colours to squares
     squares[i].style.backgroundColor = colours[i]
+
+    //add click listeners to squares
+    squares[i].addEventListener('click', function(){
+        //get colour of clicked square
+        const clickedColour = this.style.backgroundColor;
+        //compare colour to pickedColour
+        if(clickedColour === pickedColour){
+            alert('Correct!')
+        } else {
+            alert('Wrong!!!!!!!!!')
+        }
+    })
 }
+
