48cbf86b4bbe799cd41206ab65219916d2fb4713
Jay Milton <jay@Jays-MacBook-Pro.local>
2020-08-21 19:56:47 -0400

    easy/hard buttons/modes change number of squares. play again button working properly

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-let colours = generateRandomColours(6);
-
+let numSquares = 6;
+let colours = generateRandomColours(numSquares);
 const squares = document.querySelectorAll(".square");
 let pickedColour = pickColour();
 const colourDisplay = document.getElementById('colourDisplay')
 let messageDisplay = document.querySelector("#message");
 const h1 = document.querySelector("h1")
 const resetButton = document.querySelector("#reset");
+const easyBtn = document.querySelector("#easyBtn");
+const hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener('click', function(){
+    hardBtn.classList.remove('selected');
+    easyBtn.classList.add('selected');
+    numSquares = 3;
+    colours = generateRandomColours(numSquares);
+    pickedColour = pickColour();
+    colourDisplay.textContent = pickedColour;
+    for(var i = 0; i < squares.length; i++){
+        if(colours[i]){
+            squares[i].style.backgroundColor = colours[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
+    }
+})
+hardBtn.addEventListener('click', function(){
+    hardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+    numSquares = 6;
+    colours = generateRandomColours(numSquares);
+    pickedColour = pickColour();
+    colourDisplay.textContent = pickedColour;
+    for(var i = 0; i < squares.length; i++){
+            squares[i].style.backgroundColor = colours[i];
+            squares[i].style.display = 'block';
+        }
+})
 
 resetButton.addEventListener("click", function(){
     //generate all new colours
-    colours = generateRandomColours(6);
+    colours = generateRandomColours(numSquares);
     //pick new random colur from array
     pickedColour = pickColour();
     //change colourDisplay to match picked colour
@@ -18,6 +48,7 @@ resetButton.addEventListener("click", function(){
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colours[i];
     }
+    h1.style.backgroundColor = "#232323"
 })
 
 colourDisplay.textContent = pickedColour;
@@ -34,6 +65,7 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColour === pickedColour){
             changeColours(clickedColour)
             messageDisplay.textContent = "Correct!!"
+            resetButton.textContent = "Play Again?"
             h1.style.backgroundColor = pickedColour
         } else {
             this.style.backgroundColor = "#232323";
